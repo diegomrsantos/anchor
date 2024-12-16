a12a8e96fad04460e8c1bac36692120f48e904c8
diego <diego@sigmaprime.io>
2024-12-16 23:38:07 +0100

    expose Enr

--- anchor/network/src/lib.rs
+++ anchor/network/src/lib.rs
@@ -11,3 +11,5 @@ mod types;
 pub use config::Config;
 pub use lighthouse_network::{ListenAddr, ListenAddress};
 pub use network::Network;
+
+pub type Enr = discv5::enr::Enr<discv5::enr::CombinedKey>;
